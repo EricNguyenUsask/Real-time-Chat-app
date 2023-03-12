c44a17f9080733c25f6b140b3c8ee54896a37e3d
Thang Nguyen <93494432+ThangNguyenUsask@users.noreply.github.com>
2023-03-11 22:52:13 -0600

    commit

--- a4_353_dun329/src/Example/AddComponent.js
+++ a4_353_dun329/src/Example/AddComponent.js
@@ -1,66 +1,77 @@
-import React from 'react';
+import React from "react";
 
 class AddComponent extends React.Component {
+  state = {
+    postTopic: "",
+    postData: "",
+  };
+  handleChangeTopic = (event) => {
+    this.setState({
+      postTopic: event.target.value,
+    });
+  };
+  handleChangeData = (event) => {
+    this.setState({
+      postData: event.target.value,
+    });
+  };
 
-    state = {
-        postTopic: '',
-        postData: '',
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (!this.state.postTopic || !this.state.postData) {
+      alert("Missing required params");
+      return;
     }
-    handleChangeTopic = (event) => {
-        this.setState({
-            postTopic: event.target.value
-        })
-    }
-    handleChangeData = (event) => {
-        this.setState({
-            postData: event.target.value
-        })
-    }
+    const newPost = {
+      postID: Math.floor(Math.random() * 1001),
+      postTopic: this.state.postTopic,
+      postData: this.state.postData,
+    };
+    fetch("http://localhost:3000/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPost),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        this.props.addNewPost(newPost);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    this.setState({
+      postTopic: "",
+      postData: "",
+    });
+  };
 
-
-    handleSubmit = (event) => {
-        event.preventDefault()
-        if (!this.state.postTopic || !this.state.postData) {
-            alert('Missing required params')
-            return;
-        }
-        console.log('>>> check data input: ', this.state)
-        this.props.addNewPost({
-            postID: Math.floor(Math.random() * 1001),
-            postTopic: this.state.postTopic,
-            postData: this.state.postData
-        })
-
-        this.setState({
-            postTopic: '',
-            postData: ''
-        })
-    }
-
-    render() {
-        return (
-            <form>
-                <label htmlFor="fname">postTopic:</label><br />
-                <input
-                    type="text"
-                    value={this.state.postTopic}
-                    onChange={(event) => this.handleChangeTopic(event)}
-                />
-                <br />
-                <label htmlFor="lname">postData:</label><br />
-                <input
-                    type="text"
-                    value={this.state.postData}
-                    onChange={(event) => this.handleChangeData(event)}
-
-                /><br /><br />
-                <input type="submit"
-                    onClick={(event) => this.handleSubmit(event)}
-                />
-            </form>
-        )
-    }
-
+  render() {
+    return (
+      <form>
+        <label htmlFor="fname">postTopic:</label>
+        <br />
+        <input
+          type="text"
+          value={this.state.postTopic}
+          onChange={(event) => this.handleChangeTopic(event)}
+        />
+        <br />
+        <label htmlFor="lname">postData:</label>
+        <br />
+        <input
+          type="text"
+          value={this.state.postData}
+          onChange={(event) => this.handleChangeData(event)}
+        />
+        <br />
+        <br />
+        <input type="submit" onClick={(event) => this.handleSubmit(event)} />
+      </form>
+    );
+  }
 }
 
 export default AddComponent;
--- a4_353_dun329/src/Example/ListComponent.js
+++ a4_353_dun329/src/Example/ListComponent.js
@@ -1,52 +1,59 @@
-import React from 'react';
+import React from "react";
 
 class ListComponent extends React.Component {
-    //re-render
-    state = {
-        showPosts: false
-    }
+  //re-render
+  state = {
+    showPosts: false,
+  };
 
-    handleShowHide = () => {
-        this.setState({
-            showPosts: !this.state.showPosts
-        })
-    }
-    render() {
-        let { arrPost } = this.props;
-        let { showPosts } = this.state;
-        let check = showPosts === true ? 'showPosts = true' : 'showPosts = false';
-        console.log('>>> check conditional: ', check)
-        return (
-            <>
-                {showPosts === false ?
-                    <div>
-                        <button onClick={() => this.handleShowHide()}>Show</button>
-                    </div>
-                    :
-                    <>
-                        <div className="post-lists">
-                            {
-                                arrPost.map((item, index) => {
-                                    return (
-                                        <div key={item.postID}>
-                                            {item.postTopic} - {item.postData}
-                                        </div>
-                                    )
-                                })
+  handleShowHide = () => {
+    // if (!this.state.showPosts) {
+    //   fetch("http://localhost:3000/posts", {
+    //     method: "GET",
+    //   })
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //       this.setState({ posts: data });
+    //     })
+    //     .catch((error) => {
+    //       console.error("Error fetching posts:", error);
+    //     });
+    // }
+    this.setState({
+      showPosts: !this.state.showPosts,
+    });
+  };
 
-                            }
-
-                        </div>
-                        <div>
-                            <button onClick={() => this.handleShowHide()}>Hide</button>
-                        </div>
-                    </>
-                }
-            </>
-        )
-
-    }
+  render() {
+    let { arrPost } = this.props;
+    let { showPosts } = this.state;
+    let check = showPosts === true ? "showPosts = true" : "showPosts = false";
+    console.log(">>> check conditional: ", check);
+    return (
+      <>
+        {showPosts === false ? (
+          <div>
+            <button onClick={() => this.handleShowHide()}>Show</button>
+          </div>
+        ) : (
+          <>
+            <div className="post-lists">
+              {arrPost.map((item, index) => {
+                return (
+                  <div key={item.postID}>
+                    {item.postTopic} - {item.postData}
+                  </div>
+                );
+              })}
+            </div>
+            <div>
+              <button onClick={() => this.handleShowHide()}>Hide</button>
+            </div>
+          </>
+        )}
+      </>
+    );
+  }
 }
-
 
 export default ListComponent;
